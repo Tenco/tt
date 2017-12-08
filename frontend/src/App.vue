02289310c83da690c7c23affd49c893fde87f836
Eric Ericsson <eric@tenco.se>
2017-12-08 12:08:04 +0100

    Setup vue and cleanup package.json

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <main id="tencotools">
+        <h2>Hello from vue</h2>
+    </main>
+</template>
+
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+#tencotools {
+    background-color: red;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,29 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './public/js'),
         filename: 'index.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        js: 'babel-loader'
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
 };
